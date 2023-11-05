1180f08d743fa0f62140eb8711a744ef7088d690
Kaushal Mallah <kmallah21@gmail.com>
2023-11-06 05:15:41 +0530

    first commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAxGqBhHpV29nDSBJ1Dq3PKALgtN07OeSo",
+  authDomain: "crypto-coin-tracker-25441.firebaseapp.com",
+  projectId: "crypto-coin-tracker-25441",
+  storageBucket: "crypto-coin-tracker-25441.appspot.com",
+  messagingSenderId: "990721779772",
+  appId: "1:990721779772:web:ed2b1210fae541762e3b39",
+  measurementId: "G-CYJ4MBRT1J"
+};
+
+export default firebaseConfig;
